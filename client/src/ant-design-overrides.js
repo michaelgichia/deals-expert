bc25c992eeb1e405839aa145196abec21fb1b9e7
michaelgichia <gichuru.gichia@gmail.com>
2018-02-27 17:56:56 +0300

    Add fonts.

--- client/src/ant-design-overrides.js
+++ client/src/ant-design-overrides.js
@@ -3,5 +3,6 @@ module.exports = {
   '@layout-body-background': '#f8f8f8',
   '@layout-header-background': '#ffffff',
   '@layout-header-height': '110px',
-  'input-border-color': '#dcdcdc'
+  '@input-border-color': '#dcdcdc',
+  '@font-family': '"Karla", sans-serif'
 };
