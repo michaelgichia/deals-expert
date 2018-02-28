d2109824c1fc61b905704519e9d5fb597822646b
michaelgichia <gichuru.gichia@gmail.com>
2018-02-28 10:48:46 +0300

    :zap: Add fonts.~

--- client/src/ant-design-overrides.js
+++ client/src/ant-design-overrides.js
@@ -4,5 +4,9 @@ module.exports = {
   '@layout-header-background': '#ffffff',
   '@layout-header-height': '110px',
   '@input-border-color': '#dcdcdc',
-  '@font-family': '"Karla", sans-serif'
+  '@font-family': '"Karla", sans-serif',
+  '@carousel-dot-width': '6px',
+  '@carousel-dot-height': '6px',
+  '@carousel-dot-active-height': '7px',
+  '@carousel-dot-active-width': '7px'
 };
